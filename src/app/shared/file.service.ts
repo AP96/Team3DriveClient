7dbbfc0c4d07f3ed0edf6a961a85ed018e243bc5
AP96 <alon.sandberg@gmail.com>
2020-04-05 17:16:23 +0300

    added fileService

---
+++ src/app/shared/file.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileService {
+
+  constructor(private http: HttpClient) { }
+
+  getFiles(): Observable<any> {
+    return this.http.get('/api/uploadMultipleFiles');
+  }
+
+}
